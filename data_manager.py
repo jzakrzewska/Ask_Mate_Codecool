3d0dac212751dcf140ac8435a54e23cc636958d7
FabolousBishop <bartosz.tomaszewski.1994@gmail.com>
2021-01-02 13:29:49 +0100

    Dodano Display Question

--- data_manager.py
+++ data_manager.py
@@ -1,50 +1,32 @@
-import util
-
-answers_file = "sample_data/answer.csv"
-question_file = "sample_data/question.csv"
+answers_file = 'sample_data/answer.csv'
+question_file = 'sample_data/question.csv'
 
 
+in_memory_question_dictionary_keys = [
+    'Question Id', 'Submission Time', 'View Number', 'Vote Number', 'Title', 'Message', 'Image',
+]
+
+in_memory_answer_dictionary_keys = [
+    'Answer Id', 'Submission Time', 'Vote Number', 'Question Id', 'Message', 'Image',
+]
 
 
-# id_index = 0
-time_index = 1
-# view_index = 2
-# vote_index = 3
-# question_index = 4
-# message_index = 5
-# image_index = 6
-
-dictionary_keys_in_memory = ["id","submission_time","view_number","vote_number","title","message","image"]
-
-
-def read_dict_from_file(file_name, separator=','):
-
+def read_dictionary_from_file(file_name, separator=','):
     try:
-        with open(file_name, "r") as file:
+        with open(file_name, 'r') as file:
             lines = file.readlines()
 
-            listed_data = [element.replace("\n", "").replace('"', " ").split(separator) for element in lines]
-            dict_keys = listed_data[0]
-            dict_answers = listed_data[1:]
-
-            for sublist in dict_answers:
-                sublist[time_index] = util.convert_unix_to_date(int(sublist[time_index]))
+            listed_data = [element.replace('\n', '').split(separator) for element in lines]
+            dictionary_keys = listed_data[0]
+            dictionary_elements = listed_data[1:]
             all_data = []
 
-            for i in range(len(dict_answers)):
-
-                new_dict = dict(zip(dict_keys,dict_answers[i]))
-                all_data.append(new_dict)
+            for element in range(len(dictionary_elements)):
+                new_dictionary = dict(zip(dictionary_keys, dictionary_elements[element]))
+                all_data.append(new_dictionary)
 
         return all_data
 
     except IOError:
         return {}
 
-
-def write_dict_to_file(file_name,dict, separator=','):
-
-    with open(file_name, "w") as file:
-        for record in dict:
-            row = separator.join(record)
-            file.write(row + "\n")
--- server.py
+++ server.py
@@ -1,42 +1,30 @@
-from flask import Flask, render_template, url_for, request, redirect
-
-import time
+from flask import Flask, render_template
 import data_manager
-import util
+
 
 app = Flask(__name__)
-answers_file = "sample_data/answer.csv"
-question_file = "sample_data/question.csv"
 
 
-@app.route("/")
+@app.route('/')
 def index():
-
-    return render_template("index.html")
-
-
-@app.route("/list")
-def list_questions():
-    dictionary_keys = data_manager.dictionary_keys_in_memory
-    questions = util.sort_questions_from_greatest_id(data_manager.read_dict_from_file(data_manager.question_file))
-
-    return render_template("list_questions.html", headers=dictionary_keys, stories=questions)
+    return render_template('index.html')
 
 
-@app.route("/add", methods=["GET"])
-def add_user_story_get():
-    return render_template("request_form.html")
+@app.route('/question/<question_id>', methods=['GET'])
+def display_a_question(question_id):
+    question_dictionary_keys = data_manager.in_memory_question_dictionary_keys
+    answers_dictionary_keys = data_manager.in_memory_answer_dictionary_keys
 
+    question = next((item for item in data_manager.read_dictionary_from_file(data_manager.question_file) if item['Question Id'] == question_id), False)
+    answers = next((item for item in data_manager.read_dictionary_from_file(data_manager.answers_file) if item['Question Id'] == question_id), False)
 
-@app.route("/add", methods=["POST"])
-def add_user_story_post():
-    data = dict(request.form)
-    data["view number"] = 0
-    data["id"] = util.greatest_id() + 1
-    data["submission time"] = time.time()
-    data_manager.write_dict_to_file(question_file, data)
-    return redirect(url_for(list_questions))
+    return render_template(
+        'question.html',
+        question=question,
+        answers=answers,
+        question_headers = question_dictionary_keys,
+        answers_headers = answers_dictionary_keys
+    )
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
