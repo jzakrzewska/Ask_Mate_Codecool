a1a9a99f719bed987d0880c6919c77e8b3e2c39a
jzakrzewska <askazakrzewska@gmail.com>
2021-01-04 18:51:46 +0100

    add new answer added, style changed

--- server.py
+++ server.py
@@ -47,6 +47,7 @@ def display_a_question(question_id):
     answers_dictionary_keys = data_manager.dictionary_keys_in_memory_answer
 
     question = next((item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id), False)
+
     answers = list(item for item in data_manager.read_dict_from_file(answers_file) if item['question_id'] == question_id)
 
     return render_template(
@@ -57,6 +58,26 @@ def display_a_question(question_id):
         answers_headers=answers_dictionary_keys
     )
 
+@app.route("/question/<question_id>/add_new_answer", methods=["GET"])
+def add_new_answer_get(question_id):
+    question = next((item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id), False)
+    return render_template("add_new_answer.html", question=question)
+
+
+
+@app.route("/question/<question_id>/add_new_answer", methods=["POST"])
+def add_new_answer(question_id):
+    question = next((item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id), False)
+
+    answer = {"id": util.greatest_id(data_manager.read_dict_from_file(answers_file)) + 1,
+              "submission_time": int(time.time()),
+              "vote_number": 0,
+              "question_id": question_id,
+              "message": request.form.get("message"),
+              "image": ""
+              }
+    data_manager.write_dict_to_file(answers_file, answer)
+    return redirect(url_for("display_a_question",question_id=question_id))
 
 if __name__ == "__main__":
     app.run()
--- util.py
+++ util.py
@@ -6,8 +6,8 @@ def sort_questions_from_greatest_id(data):
     int_list = []
     for text in data:
         text["id"] = int(text["id"])
-        text["view_number"] = int(text["view_number"])
-        text["vote_number"] = int(text["vote_number"])
+        #text["view_number"] = int(text["view_number"])
+        #text["vote_number"] = int(text["vote_number"])
         int_list.append(text)
     sorted_data = sorted(int_list, key=lambda d: d["id"], reverse=True)
 
