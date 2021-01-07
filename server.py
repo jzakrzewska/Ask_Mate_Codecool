b6334537b2a6758eb1a86c18be0f1545f1c77d2e
FabolousBishop <bartosz.tomaszewski.1994@gmail.com>
2021-01-07 19:42:24 +0100

    Tabelka gotowa, głosowanie wymaga poprawy (czas sprawia problem).

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ question_file = "sample_data/question.csv"
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
 
 
@@ -41,14 +40,18 @@ def add_question_post():
     data_manager.add_dict_to_file(question_file, data)
     return redirect(url_for("index"))
 
+
 @app.route('/question/<question_id>', methods=['GET'])
 def display_a_question(question_id):
     question_dictionary_keys = data_manager.dictionary_keys_in_memory_question
     answers_dictionary_keys = data_manager.dictionary_keys_in_memory_answer
 
-    question = next((item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id), False)
+    question = next(
+        (item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id),
+        False)
 
-    answers = list(item for item in data_manager.read_dict_from_file(answers_file) if item['question_id'] == question_id)
+    answers = list(
+        item for item in data_manager.read_dict_from_file(answers_file) if item['question_id'] == question_id)
 
     return render_template(
         'question.html',
@@ -56,7 +59,8 @@ def display_a_question(question_id):
         answers=answers,
         question_headers=question_dictionary_keys,
         answers_headers=answers_dictionary_keys
-        )
+    )
+
 
 @app.route('/question/<question_id>/delete', methods=["GET"])
 def delete_a_question_get(question_id):
@@ -79,14 +83,17 @@ def delete_a_question_get(question_id):
 
 @app.route("/question/<question_id>/add_new_answer", methods=["GET"])
 def add_new_answer_get(question_id):
-    question = next((item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id), False)
+    question = next(
+        (item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id),
+        False)
     return render_template("add_new_answer.html", question=question)
-
 
 
 @app.route("/question/<question_id>/add_new_answer", methods=["POST"])
 def add_new_answer(question_id):
-    question = next((item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id), False)
+    question = next(
+        (item for item in data_manager.read_dict_from_file(data_manager.question_file) if item['id'] == question_id),
+        False)
 
     answer = {"id": util.greatest_id(data_manager.read_dict_from_file(answers_file)) + 1,
               "submission_time": int(time.time()),
@@ -100,7 +107,7 @@ def add_new_answer(question_id):
 
 
 @app.route("/answer/<answer_id>/delete/<question_id>", methods=["GET"])
-def delete_an_answer(answer_id,question_id):
+def delete_an_answer(answer_id, question_id):
     answers = data_manager.read_dict_from_file(data_manager.answers_file)
 
     answers_after_deletion = list(d for d in answers if d["id"] != answer_id)
@@ -109,19 +116,23 @@ def delete_an_answer(answer_id,question_id):
     return redirect(url_for("display_a_question", question_id=question_id))
 
 
-@app.route('/answer/<answer_id>/vote_up', methods=['GET'])
+@app.route('/answer/<answer_id>/vote_up/<question_id>', methods=['GET'])
 def vote_up_answer(answer_id, question_id):
     answers = data_manager.read_dict_from_file(data_manager.answers_file)
     answers_dictionary_keys = data_manager.dictionary_keys_in_memory_answer
 
-    answer = next((item for item in data_manager.read_dict_from_file(data_manager.answers_file) if item['id'] == answer_id), False)
-    answer['vote_number'] = answer['vote_number'] + 1
+    answer = next(
+        (item for item in data_manager.read_dict_from_file(data_manager.answers_file) if item['id'] == answer_id),
+        False)
+    answer['vote_number'] = str(int(answer['vote_number']) + 1)
 
-    answers_after_deletion
+    answers_after_deletion = list(d for d in answers if d["id"] != answer_id)
+    data_manager.write_data_to_file(data_manager.answers_file, answers_after_deletion)
 
-    data_manager.add_data_to_file(data_manager.answers_file, answer)
+    data_manager.add_dict_to_file(data_manager.answers_file, answer)
 
     return redirect(url_for('display_a_question', question_id=question_id))
+
 
 if __name__ == "__main__":
     app.run()
