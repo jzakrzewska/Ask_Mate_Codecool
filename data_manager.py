33a63a829d4b05d12028513e47514a2169131e16
Slawomir-J <nonseyy@gmail.com>
2021-01-16 15:28:42 +0100

    próbuje zrobić add question

--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,7 @@ from typing import List, Dict
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 import connection
-import time
+import datetime
 
 @connection.connection_handler
 def get_id(cursor: RealDictCursor) -> list:
@@ -25,13 +25,13 @@ def add_question(cursor: RealDictCursor, question) -> list:
     command = """
             INSERT INTO question (id, image, message, title, vote_number, view_number, submission_time)
             VALUES %(id)s,%(image)s,%(message)s,%(title)s,%(vote_number)s,%(view_number)s,%(submission_time)s);"""
-    param = {'id': "3",
-            'image': None,
+    param = {'id': 3,
+             'image': None,
              'message': question.get("massage"),
              'title': question.get("title"),
-             'vote_number': "0",
-             'view_number': "0",
-             'submission_time': int(time.time())}
+             'vote_number': 0,
+             'view_number': 0,
+             'submission_time': datetime.datetime.now()}
     cursor.execute(command, param)
 # answers_file = "sample_data/answer.csv"
 # question_file = "sample_data/question.csv"
